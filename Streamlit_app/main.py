a736cc5c89b59ecd35729b96909a4947299eb0c4
Oscar Ahumada Gómez <116230587+OscarAhumadaG@users.noreply.github.com>
2024-03-07 06:37:37 -0500

    Update main.py

--- Streamlit_app/main.py
+++ Streamlit_app/main.py
@@ -4,15 +4,22 @@ import streamlit as st
 from streamlit_image_select import image_select
 
 import numpy as np
-import imageio
+# import imageio
+import imageio.v3 as iio
 from PIL import Image
 import os
+import io
 import warnings
 import shutil
 import time
 import base64
+import inspect
 
 warnings.simplefilter("ignore")
+
+raw_frames = []
+
+@st.cache_data()
 def image_to_matrix(image_path):
     # Open the image
     img = Image.open(image_path)
@@ -25,6 +32,9 @@ def image_to_matrix(image_path):
 
     matrix = [[0 if img.getpixel((x, y)) == 255 else 1 for x in range(width)] for y in range(height)]
     return np.array(matrix)
+
+
+@st.cache_data()
 def matrix_to_image(matrix, counter, file_name=None):
     scale = 300 // matrix.shape[0]
 
@@ -47,11 +57,13 @@ def matrix_to_image(matrix, counter, file_name=None):
             else:
                 img.putpixel((x, y), (0, 255, 0))
 
-    img.save(f"{file_name}/{counter}.png", quality=99)
+    # img.save(f"{file_name}/{counter}.png", quality=99)
+    return img
+
 
 def step(i, j, fn, matrix=None, deep_path=[], final_path=[], end_point=(0, 0)):
     global counter
-
+    global raw_frames
     if matrix[end_point] > 0:
         return
 
@@ -65,12 +77,13 @@ def step(i, j, fn, matrix=None, deep_path=[], final_path=[], end_point=(0, 0)):
     final_path.append((i, j))
     counter += 1
     matrix[(i, j)] = counter
-    matrix_to_image(matrix, counter, file_name=fn)
+    frame = matrix_to_image(matrix, counter, file_name=fn)
+    raw_frames.append(frame)
 
     if (i, j) == end_point:
         st.write(f"This is the deep path: {deep_path}\n")
         st.write(f"\nThis is the final path: {final_path}")
-        return
+        return raw_frames
 
 
     else:
@@ -81,56 +94,80 @@ def step(i, j, fn, matrix=None, deep_path=[], final_path=[], end_point=(0, 0)):
         if (i, j) in deep_path or matrix[(i, j)] > 0:
             matrix[(i, j)] = -2
             final_path.pop()
-            return
+            return raw_frames
+
+
 def main():
     st.title("Maze")
-    origin_img = ["Maze1.png","Maze2.png"]
     start_end = {'Maze1':[(0, 4),(12, 7)],'Maze2':[(1, 1),(2, 5)]}
+
+    current_dir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
+    # parent_dir = os.path.dirname(current_dir)
+    maze1_resize = current_dir + "/Maze1_resize.png"
+    maze2_resize = current_dir + "/Maze2_resize.png"
+    origin_img = [current_dir + "/Maze1.png",
+                  current_dir + "/Maze2.png"]
+    ## check the path
+    # st.write(parent_dir)
+    # st.write(maze1_resize)
+
+    # files = os.listdir(parent_dir)
+    # for file in files:
+    #    st.write(file)
 
     img = image_select(
         label="Select a maze",
-        images=["Maze1_resize.png", "Maze2_resize.png"],
+        images=[maze1_resize, maze2_resize],
         captions=["Maze 1", "Maze 2"],
         use_container_width = False,
         return_value ='index'
     )
     image_path = origin_img[img]
 
+
+
     if st.button("Run Deep Search"):
         maze1 = image_to_matrix(image_path)
         image_name2 = os.path.splitext(os.path.basename(image_path))[0]
-        try:
-            os.makedirs(image_name2)
-        except FileExistsError:
-            # directory already exists
-            with st.spinner("Erasing previous directory"):
-                shutil.rmtree(image_name2)
-                time.sleep(2)
 
-            os.makedirs(image_name2)
+        with st.spinner("Searching path"):
+
+            time.sleep(2)
+
+        #os.makedirs(image_name2)
 
         start = start_end[image_name2][0]
         end_point = start_end[image_name2][1]
 
         global counter
         counter = 1
-        step(start[0], start[1], fn=image_name2, matrix=maze1, end_point=end_point)
+        raw_frames = step(start[0], start[1], fn=image_name2, matrix=maze1, end_point=end_point)
 
 
+        # images = [iio.imread(f'{image_name2}/{i}.png') for i in range(2, counter + 1)]
+        images = [np.asarray(fr) for fr in raw_frames]
 
-        images = [imageio.imread(f'{image_name2}/{i}.png') for i in range(2, counter + 1)]
-        imageio.mimsave(f'{image_name2}/path.gif', images, duration=5.5)
+        # imageio.mimsave(f'{image_name2}/path.gif', images, duration=5.5)
+
+        # instead of saving the file locally, and then loading it,
+        # we directly store it in the binary format in memory
+        bytes_image = iio.imwrite("<bytes>", images, duration=5.5, loop=0, extension=".gif")
+        frames = iio.imread(bytes_image, index=None)
+        byte_stream = io.BytesIO(bytes_image)
+        contents = byte_stream.read()
+
 
         """### Solution"""
-        file_ = open(f'{image_name2}/path.gif', "rb")
-        contents = file_.read()
+        # file_ = open(f'/{image_name2}/path.gif', "rb")
+        # contents = file_.read()
         data_url = base64.b64encode(contents).decode("utf-8")
-        file_.close()
+        # file_.close()
 
         st.markdown(
             f'<img src="data:image/gif;base64,{data_url}" alt="path gif">',
             unsafe_allow_html=True,
         )
+
 
 if __name__ == "__main__":
     main()
